ed1e63a9ad45442d185ed6e4a8a0bb2e934bff12
DanjerSnekker <viperrules4ever@gmail.com>
2024-08-09 21:06:10 +0400

    Remade Player with Enhanced Input
    Nothing is broken. For now...
    Player has a 3rd person camera.
    Player can walk in 4 directions.
    Player can jump.
    Player's rotation is tied to the camera.

--- Source/CH_GoWClone/Player/Chara.cpp
+++ Source/CH_GoWClone/Player/Chara.cpp
@@ -1,5 +1,10 @@
 #include "CH_GoWClone/Player/Chara.h"
-
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 AChara::AChara()
@@ -7,6 +12,20 @@ AChara::AChara()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Initialize SpringArm
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("Spring Arm");
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->bUsePawnControlRotation = true;
+
+	//Initialize Camera
+	Cam = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Cam->SetupAttachment(SpringArm);
+
+	//Ensures camera rotation will rotate the character.
+	bUseControllerRotationYaw = true;
+	
+	//Ensures character doesn't rotate based on movement direction.
+	GetCharacterMovement()->bOrientRotationToMovement = false;
 }
 
 // Called when the game starts or when spawned
@@ -28,5 +47,62 @@ void AChara::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	//Add input mapping context
+	if (APlayerController* PController = Cast<APlayerController>(Controller))
+	{
+		//Get local player subsystem
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PController->GetLocalPlayer()))
+		{
+			//Add input context
+			Subsystem->AddMappingContext(InputMapping, 0);
+		}
+	}
+
+	if (UEnhancedInputComponent* Input = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		Input->BindAction(CharaMove, ETriggerEvent::Triggered, this, &AChara::MoveChara);
+
+		Input->BindAction(CharaLook, ETriggerEvent::Triggered, this, &AChara::LookChara);
+
+		Input->BindAction(CharaJump, ETriggerEvent::Triggered, this, &AChara::JumpChara);
+	}
 }
+
+void AChara::MoveChara(const FInputActionValue& InputValue)
+{
+	FVector2D InputVector = InputValue.Get<FVector2D>();
+
+	if (IsValid(Controller))
+	{
+		//Get forward direction
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector ForwardDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		//Add movement
+		AddMovementInput(ForwardDir, InputVector.Y);
+		AddMovementInput(RightDir, InputVector.X);
+	}
+}
+
+void AChara::LookChara(const FInputActionValue& InputValue)
+{
+	FVector2D InputVector = InputValue.Get<FVector2D>();
+
+	if (IsValid(Controller))
+	{
+		AddControllerYawInput(InputVector.X);
+		AddControllerPitchInput(InputVector.Y);
+	}
+}
+
+void AChara::JumpChara()
+{
+	AChara::Jump();
+}
+
+
+
 
--- Source/CH_GoWClone/Player/Chara.h
+++ Source/CH_GoWClone/Player/Chara.h
@@ -2,12 +2,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "Chara.generated.h"
 
 UCLASS()
 class CH_GOWCLONE_API AChara : public ACharacter
 {
 	GENERATED_BODY()
+//-------------------------MY CODE-------------------------
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* Cam;
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* SpringArm;
+//-------------------------MY CODE-------------------------
 
 public:
 	// Sets default values for this character's properties
@@ -24,9 +32,23 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+//-------------------------MY CODE-------------------------
 protected:
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = "Enhanced Input")
+	class UInputMappingContext* InputMapping;
 	
-	
+	UPROPERTY(EditAnywhere, Category = "Enhanced Input")
+	class UInputAction* CharaMove;
+
+	UPROPERTY(EditAnywhere, Category = "Enhanced Input")
+	class UInputAction* CharaJump;
+
+	UPROPERTY(EditAnywhere, Category = "Enhanced Input")
+	class UInputAction* CharaLook;
+
+	void MoveChara(const FInputActionValue& InputValue);
+	void LookChara(const FInputActionValue& InputValue);
+	void JumpChara();
+//-------------------------MY CODE-------------------------
 };
