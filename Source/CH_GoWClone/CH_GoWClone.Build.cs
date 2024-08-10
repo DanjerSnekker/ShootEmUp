45ed4623bd5b28c63a1e1039bf407a6a9912f4bd
DanjerSnekker <viperrules4ever@gmail.com>
2024-08-10 14:13:41 +0400

    Just saving a bit of progress

--- Source/CH_GoWClone/CH_GoWClone.Build.cs
+++ Source/CH_GoWClone/CH_GoWClone.Build.cs
@@ -8,7 +8,19 @@ public class CH_GoWClone : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] 
+		{ 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput",
+			"Niagara",
+			"AIModule",
+			"NavigationSystem",
+			"GameplayTasks",
+			"UMG"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
